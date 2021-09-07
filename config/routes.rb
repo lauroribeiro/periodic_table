16c63d2d746c930b5c604ea89ddfddddb934526f
Lauro Ribeiro <lauroribeirola@hotmail.com>
2021-09-07 14:50:31 +0000

    change routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
-  root to: "welcome#index"
-  resources :table, only: %i[index show]
+  # root to: "welcome#index"
+  # resources :table, only: %i[index show]
+  root to: "elements#index"
+  resources :elements, only: %i[index show], path: "/table/", param: :name
 end
