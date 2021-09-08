22e934c6e516b680a36bec5e9523744931db6bd6
Lauro Ribeiro <lauroribeirola@hotmail.com>
2021-09-08 22:50:42 +0000

    update table layout

--- app/javascript/packs/elements.js
+++ app/javascript/packs/elements.js
@@ -1,2 +1,6 @@
-elements = Array.from(document.querySelectorAll(".element"));
+// elements = Array.from(document.querySelectorAll(".element"));
 
+// elements.forEach(e=>{
+//     let arr = Array.from(e.classList)[1];
+//     console.log(arr);
+// })
