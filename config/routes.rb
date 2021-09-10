77cb95bd15e3fc9951c04cff38ac8780ad2ba223
Lauro Ribeiro <lauroribeirola@hotmail.com>
2021-09-10 00:59:15 +0000

    improve routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  # root to: "welcome#index"
-  # resources :table, only: %i[index show]
   root to: "elements#index"
   resources :elements, only: %i[index show], path: "/", param: :name
 end
